4209c9d7acbef16964d6afc0bf2f59c13c4051b0
swordabs <91110269+swordabs@users.noreply.github.com>
2022-03-16 01:10:13 -0700

    Add files via upload

---
+++ demo3/src/main/java/com/example/demo3/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.demo3;
+
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+    }
+}
